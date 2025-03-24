4fcc8a73569d416606dac272ce962014b9dd45aa
Hushanpreet77 <Hushanpreetkaur86@gmail.com>
2025-03-24 20:24:27 +0000

    update

---
+++ lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const Mainapp());
+}
+
+class Mainapp extends StatelessWidget {
+  const Mainapp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(home: HomePage());
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(backgroundColor: Colors.blue),
+      drawer: Drawer(),
+      body: Container(color: Colors.white),
+      bottomNavigationBar: BottomAppBar(),
+    );
+  }
+}
